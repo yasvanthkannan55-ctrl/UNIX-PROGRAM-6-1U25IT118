a360bb086cb53f5fc7771017087f8384c202c2bf
YASVANTH KANNAN L <yasvanthkannan55@gmail.com>
2026-09-07 13:04:27 +0530

    UNIX PROGRAM 6 IU25IT118

--- student.sh
+++ student.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+# Create newfile.txt
+echo "Hello Linux" > newfile.txt
+echo "This is a cat command practical." >> newfile.txt
+
+# Display newfile.txt
+cat newfile.txt
+
+# Create file1.txt
+echo "First file" > file1.txt
+
+# Create file2.txt
+echo "Second file" > file2.txt
+
+# Concatenate file1.txt and file2.txt
+cat file1.txt file2.txt
+
+# Create existingfile.txt
+echo "Existing file" > existingfile.txt
+
+# Append file1.txt to existingfile.txt
+cat file1.txt >> existingfile.txt
+
+# Display existingfile.txt
+cat existingfile.txt
+
+# Number the lines of newfile.txt
+cat -n newfile.txt
